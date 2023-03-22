0e1a774420e8f791eff1d9f51de2e62ad6c58533
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-03-22 21:41:11 +0800

    add custom colors and font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,19 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-blue": "hsl(209, 23%, 22%)",
+        "very-dark-blue": "hsl(207, 26%, 17%)",
+        "light-mode-very-dark-blue": "hsl(207, 26%, 17%)",
+        "light-mode-dark-gray": "hsl(0, 0%, 52%)",
+        "light-mode-light-gray": "hsl(0, 0%, 98%)",
+      },
+
+      fontFamily: {
+        "nunito": ['Nunito Sans', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }

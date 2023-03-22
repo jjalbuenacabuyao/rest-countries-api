0bb0714f3b5ab99d29febdd328f0da9549daca84
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-03-22 21:41:51 +0800

    use react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Homepage } from './containers'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )

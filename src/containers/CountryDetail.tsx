2f382f44d53bb4adad01ed6da4e57ab366f717fa
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-03-22 21:42:40 +0800

    initial commit

---
+++ src/containers/CountryDetail.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function CountryDetail({}: Props) {
+  return (
+    <div>CountryDetail</div>
+  )
+}
+
+export default CountryDetail
---
+++ src/containers/index.ts
@@ -0,0 +1,7 @@
+import CountryDetail from "./CountryDetail";
+import Homepage from "./Homepage";
+
+export {
+  CountryDetail,
+  Homepage,
+}

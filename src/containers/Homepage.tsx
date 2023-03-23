cb20003488db294fceb03346ac155b838b0e9014
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-03-23 20:48:33 +0800

    render Header component

--- src/containers/Homepage.tsx
+++ src/containers/Homepage.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Header } from '../components'
 
 type Props = {}
 
 const Homepage = (props: Props) => {
   return (
-    <div>Homepage</div>
+    <Header />
   )
 }
 

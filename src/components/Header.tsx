47c46b40fc475c8c0eee98c0a90e3ec28ac44ede
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-03-23 20:46:20 +0800

    initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react'
+import { MdDarkMode, MdLightMode } from "react-icons/md"
+import FLex from './Flex'
+
+const Header = () => {
+  const [theme, setTheme] = useState("dark");
+
+  useEffect(() => {
+    const rootElement = document.documentElement.classList;
+    theme === "dark" ? rootElement.add("dark") 
+                     : rootElement.remove("dark");
+  }, [theme])
+
+  return (
+    <header className='px-5 py-8'>
+      <FLex className='justify-between'>
+        <a href="#" className='font-bold'>
+          Where in the world?
+        </a>
+        <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+          {theme === "dark" ? <MdLightMode size="1.5rem" /> : <MdDarkMode size="1.5rem" />
+          }
+        </button>
+      </FLex>
+    </header>
+  )
+}
+
+export default Header

689ef0718d19f375effa9fe79c5175f0ee79329a
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-03-24 21:22:37 +0800

    fetch data

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,10 +1,22 @@
-import React, { useContext } from 'react'
+import React, { useEffect, useState } from 'react'
 
 type Props = {
   filter: string,
 }
 
-const Main = (props: Props) => {
+const Main = ({ filter }: Props) => {
+  const url: string = filter;
+  const defaultUrl: string = "https://restcountries.com/v3.1/all?fields=name,population,region,subregion,capital,tld,currencies,flag,languages";
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    async function getData(url : string) {
+      const data = await fetch(url).then(response => response.json())
+      setData(data);
+    }
+    url !== "" ? getData(url) : getData(defaultUrl);
+  }, [url])
+
   return (
     <main></main>
   )

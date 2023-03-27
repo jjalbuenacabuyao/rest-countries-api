dc03900c21b56622eeabd279f5f5312ace97c78f
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-03-27 10:24:46 +0800

    use axios in fetching data

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,24 +1,29 @@
 import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 
 type Props = {
   filter: string,
 }
 
 const Main = ({ filter }: Props) => {
+  // for filters : subregion,tld,currencies,flag,languages
   const url: string = filter;
-  const defaultUrl: string = "https://restcountries.com/v3.1/all?fields=name,population,region,subregion,capital,tld,currencies,flag,languages";
-  const [data, setData] = useState({});
+  const defaultUrl: string = "https://restcountries.com/v3.1/all?fields=name,population,region,capital";
+  const [data, setData] = useState<Array<object>>([]);
 
   useEffect(() => {
-    async function getData(url : string) {
-      const data = await fetch(url).then(response => response.json())
-      setData(data);
+    async function getData(url: string) {
+      const response = await axios(url);
+      setData(response.data);
     }
     url !== "" ? getData(url) : getData(defaultUrl);
   }, [url])
 
+  const [country1, country2, country3, country4, country5, country6] = data;
+
   return (
-    <main></main>
+    <main>
+    </main>
   )
 }
 

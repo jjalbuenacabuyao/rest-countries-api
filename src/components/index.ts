a822e4208926ec3fcb1fc9b372a92f302cc6f9a6
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-03-27 10:44:32 +0800

    import and export Card

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,10 +2,12 @@ import FLex from "./Flex";
 import Header from "./Header";
 import SearchAndFilter from "./SearchAndFilter";
 import Main from "./Main";
+import Card from "./Card";
 
 export {
   FLex,
   Header,
   SearchAndFilter,
   Main,
+  Card,
 }

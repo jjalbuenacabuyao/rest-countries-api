2319d98d68d33c0b5f2aff7df960ef0b5962ba04
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-03-28 21:34:22 +0800

    created component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import Flex from './Flex'
 
-type Props = {}
+type Props = {
+  countryName: string,
+  population: number,
+  region: string,
+  capital: Array<string>,
+  flag: string,
+  alt: string,
+}
 
-const Card = (props: Props) => {
+const Card = ({countryName, population, region, capital, flag, alt}: Props) => {
   return (
-    <div>Card</div>
+    <Flex direction='col'>
+      <div>
+        <img src={flag} alt={alt} />
+      </div>
+
+      <Flex direction='col'>
+        <p>{countryName}</p>
+        <p>Population: {population}</p>
+        <p>Region: {region}</p>
+        <p>Capital: {capital}</p>
+      </Flex>
+    </Flex>
   )
 }
 

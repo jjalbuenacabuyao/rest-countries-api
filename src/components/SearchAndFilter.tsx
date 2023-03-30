5a6c5facb6d3faa6ff7afe0e3fa3f64c0bdd0de3
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-03-30 20:02:28 +0800

    change the name of the prop

--- src/components/SearchAndFilter.tsx
+++ src/components/SearchAndFilter.tsx
@@ -2,22 +2,22 @@ import React from 'react'
 import FLex from './Flex'
 
 type Props = {
-  setFilter: React.Dispatch<React.SetStateAction<string>>,
+  setApiUrl: React.Dispatch<React.SetStateAction<string>>,
 }
 
-const SearchAndFilter = ({setFilter}: Props) => {
+const SearchAndFilter = ({ setApiUrl }: Props) => {
   return (
     <FLex>
       <input
         type="search"
         name="search"
         id="search"
-        onChange={(e) => setFilter(`https://restcountries.com/v3.1/name/${e.target.value}`)} />
+        onChange={(e) => setApiUrl(`https://restcountries.com/v3.1/name/${e.target.value}`)} />
 
       <select
         name="Filter by Region"
         id="filter"
-        onChange={(e) => setFilter(`https://restcountries.com/v3.1/region/${e.target.value}`)}>
+        onChange={(e) => setApiUrl(`https://restcountries.com/v3.1/region/${e.target.value}`)}>
         <option value="africa">Africa</option>
         <option value="america">America</option>
         <option value="asia">Asia</option>

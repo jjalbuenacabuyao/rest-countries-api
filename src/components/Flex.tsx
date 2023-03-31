c306d37a4e221361ead427594d7036677b5b1cea
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-03-31 12:13:37 +0800

    add default values

--- src/components/Flex.tsx
+++ src/components/Flex.tsx
@@ -1,16 +1,22 @@
 import React from 'react'
 
 type Props = {
-  direction?: string,
+  direction?: "flex-row" | "flex-row-reverse" | "flex-col" | "flex-col-reverse",
   gap?: string,
-  justify?: string,
-  children: JSX.Element | JSX.Element[],
+  justify?: "justify-normal" | "justify-start" | "justify-end" | "justify-center" | "justify-between" | "justify-around" | "justify-evenly" | "justify-stretch",
+  children?: JSX.Element | JSX.Element[],
   className?: string,
 }
 
-const Flex = ({ direction, gap, justify, children, className }: Props) => {
+const Flex = ({ 
+  direction = "flex-row",
+  gap = "gap-0",
+  justify = "justify-normal",
+  children,
+  className
+}: Props) => {
   return (
-    <div className={`flex ${direction && `flex-${direction}`} ${justify && `justify-${justify}`} ${gap && `gap-${gap}`} items-center ${className}`}>
+    <div className={`flex items-center ${direction} ${justify} ${gap} ${className}`}>
       {children}
     </div>
   )

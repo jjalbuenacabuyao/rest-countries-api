5cfeb782e5fa3fa43a28d06d35f40a84852f59b2
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-04-01 20:26:38 +0800

    add style to search

--- src/components/SearchAndFilter.tsx
+++ src/components/SearchAndFilter.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Flex from "./Flex";
+import { HiOutlineSearch } from "react-icons/hi";
 
 type Props = {
   setApiUrl: React.Dispatch<React.SetStateAction<string>>;
@@ -25,17 +26,22 @@ const SearchAndFilter = ({ setApiUrl }: Props) => {
     "?fields=name,population,region,capital,flags,subregion,tld,currencies,languages";
 
   return (
-    <Flex>
-      <input
-        type="search"
-        name="search"
-        id="search"
-        onChange={(e) =>
-          setApiUrl(
-            `https://restcountries.com/v3.1/name/${e.target.value}/${fields}`
-          )
-        }
-      />
+    <Flex direction="flex-col" align="item-stretch" gap="gap-9" className="px-4 pt-6" >
+      <div className="relative">
+        <input
+          type="search"
+          name="search"
+          id="search"
+          placeholder="Search for a country..."
+          onChange={(e) =>
+            setApiUrl(
+              `https://restcountries.com/v3.1/name/${e.target.value}/${fields}`
+            )
+          }
+          className="dark:bg-dark-blue w-full pl-16 py-4 rounded-md"
+        />
+        <HiOutlineSearch className="absolute inset-y-0 left-7 my-auto" />
+      </div>
 
       <select
         name="Filter by Region"
@@ -45,6 +51,7 @@ const SearchAndFilter = ({ setApiUrl }: Props) => {
             `https://restcountries.com/v3.1/region/${e.target.value}/${fields}`
           )
         }
+        className="dark:bg-dark-blue"
       >
         {options}
       </select>

aecab14a88e94f690ed27bd7883c58b2acc971b2
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-04-05 18:37:55 +0800

    render App

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { Homepage } from './containers'
 import './index.css'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Homepage />
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
 )

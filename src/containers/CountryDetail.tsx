300ad4eb56e0eef8612308b3e95129c59aa1e863
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-04-06 10:04:33 +0800

    use useParams for dynamic routing

--- src/containers/CountryDetail.tsx
+++ src/containers/CountryDetail.tsx
@@ -1,12 +1,8 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios';
-import { Header } from '../components';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Header } from "../components";
+import { Link, useParams } from "react-router-dom";
 import { BsArrowLeft } from "react-icons/bs";
-
-type Props = {
-  country: string;
-}
 
 type CountryInfo = {
   capital: string[];
@@ -21,11 +17,13 @@ type CountryInfo = {
   borders: string[];
 };
 
-function CountryDetail({ country }: Props) {
+function CountryDetail() {
+  const { countryCode } = useParams(); //returns the current path
 
   const baseUrl: string = "https://restcountries.com/v3.1/alpha";
-  const fields: string = "?fields=name,population,region,capital,flags,subregion,tld,currencies,languages,borders";
-  const url: string = `${baseUrl}/${country}/${fields}`;
+  const fields: string =
+    "?fields=name,population,region,capital,flags,subregion,tld,currencies,languages,borders";
+  const url: string = `${baseUrl}/${countryCode}/${fields}`;
 
   const [data, setData] = useState<CountryInfo[]>([]);
 
@@ -36,10 +34,9 @@ function CountryDetail({ country }: Props) {
     };
 
     fetchData();
-  }, [url])
+  }, [url]);
 
   console.log(data);
-  
 
   return (
     <>
@@ -51,12 +48,11 @@ function CountryDetail({ country }: Props) {
         </Link>
 
         <div>
-          <div>
-          </div>
+          <div></div>
         </div>
       </main>
     </>
-  )
+  );
 }
 
-export default CountryDetail
+export default CountryDetail;
